bf85da7a38f14a6171bab79b568178ef8bd5ed94
odddouglas <18718048671@gmial.com>
2024-11-12 20:54:19 +0800

    build: first version

---
+++ rtrepack.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024 odddouglas
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2024-11-12     odddouglas   the first version
+ */
+
+#ifndef __RT_REPACK_H__
+#define __RT_REPACK_H__
+
+#include <rtdbg.h>
+#include <board.h>
+#include <rtthread.h>
+#include <drv_gpio.h>
+#include <rtdef.h>
+#include <rtconfig.h>
+
+#endif
